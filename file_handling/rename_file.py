f449445ced0211e50014d8c7b99e852187893c1a
gaoyueanxun <3117188561@qq.com>
2023-08-26 16:24:29 +0800

    feat: write file old name dict into json file

--- file_handling/rename_file.py
+++ file_handling/rename_file.py
@@ -7,15 +7,19 @@
 '''
 
 import os
+import sys
 import shutil
 import re
+import json
 from pypinyin import pinyin, Style
+
 
 def remove_special_characters(string):
     """
     删除字符串中的空格和特殊字符
     """
     return re.sub(r'\s+|[^\w\s.]', '', string)
+
 
 def convert_to_pinyin(string):
     """
@@ -24,7 +28,8 @@ def convert_to_pinyin(string):
     pinyin_list = pinyin(string, style=Style.NORMAL)
     return ''.join([item[0] for item in pinyin_list])
 
-def rename_folder_and_files(root_path):
+
+def rename_folder_and_files(root_path, json_file):
     """
     递归地重命名文件夹及文件
     """
@@ -35,41 +40,50 @@ def rename_folder_and_files(root_path):
             new_folder_name = convert_to_pinyin(new_folder_name)
             new_folder_path = os.path.join(root_path, new_folder_name)
             os.rename(entry.path, new_folder_path)
-
-            # 创建并保存原始文件夹名称的描述文件
-            description_file_path = os.path.join(new_folder_path, 'description.txt')
-            with open(description_file_path, 'w') as f:
-                f.write(entry.name)
-            
-            # 递归处理子文件夹
-            rename_folder_and_files(new_folder_path)
+            w_str = f"\"{new_folder_name}\":\"{entry.path}\""
+            write_in_json(json_file, w_str)
+            rename_folder_and_files(new_folder_path, json_file)
         elif entry.is_file():
             # 处理文件
             file_name, file_ext = os.path.splitext(entry.name)
             new_file_name = remove_special_characters(file_name)
             new_file_name = convert_to_pinyin(new_file_name)
+            new_file_no_ext=os.path.join(root_path, new_file_name)
             new_file_name = new_file_name + file_ext
             new_file_path = os.path.join(root_path, new_file_name)
+            w_str = f"\"{new_file_no_ext}\":\"{file_name}\""
+            write_in_json(json_file, w_str)
             os.rename(entry.path, new_file_path)
 
-            if file_ext == '.m3u8':
-                # 处理 m3u8 文件中需要播放的 .ts 文件名
-                with open(new_file_path, 'r', encoding='utf-8') as f:
-                    content = f.read()
-                    
-                # 正则表达式模式，匹配中文标点符号
-                punctuation_pattern = r'[\u3000-\u303f\uff01-\uff0f\uff1a-\uff20\uff3b-\uff40\uff5b-\uff65\uffe5]'
-                
-                # 删除中文标点符号
-                content = re.sub(punctuation_pattern, '', content)
-                
-                # 将中文转化为拼音
-                pinyin_content = pinyin(content, style=Style.NORMAL)
-                pinyin_content = ''.join([item[0] for item in pinyin_content])
-                
-                with open(new_file_path, 'w', encoding='utf-8') as f:
-                    f.write(pinyin_content)
+
+def write_in_json(json_file, value):
+    with open(json_file, 'a', encoding='utf-8') as f:
+        f.write(value + ",\n")
+
+
+def read_json():
+    with open('./data.json', 'r', encoding='utf-8') as f:
+        data = json.load(f)
+    return data
+
+
+# def rename_folder_and_files(root_path):
+#     # 获取文件夹中所有文件
+#     file_list = os.listdir(root_path)
 
 if __name__ == "__main__":
-    root_directory = '/mnt/f/file/点播'
-    rename_folder_and_files(root_directory)
+    root_path = ''
+    json_file = "json/rename0826.json"
+    if len(sys.argv) > 1:
+        root_path = sys.argv[1]
+        with open(json_file, 'a', encoding='utf-8') as f:
+            f.write("{\n")
+        rename_folder_and_files(root_path, json_file)
+        with open(json_file, 'a', encoding='utf-8') as f:
+            f.write("\"None\":\"None\"\n}")
+    else:
+        root_path = ""
+        print("Please input root dir")
+    # root_directory = '/mnt/f/file/点播'
+    # json_file = "json/rename.json"
+    # write_in_json(json_file)

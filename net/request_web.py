6bc443419b9410c91a6994aacbe4a96d58291b89
gaoyueanxun <3117188561@qq.com>
2023-08-30 14:47:39 +0800

    feat: post request

--- net/request_web.py
+++ net/request_web.py
@@ -1,19 +1,35 @@
 #!/usr/bin/python3
 #coding:utf-8
 '''
-request 网站
+request 接口
 '''
 
 import requests
 
-if __name__ == "__main__":
-    headers = {
-        "Cookie":
-        f""
-    }
+
+def post_req():
+    response = requests.post("http://localhost:6003/generate_token")
+    token = response.json().get("token")
+    print(response.json())
+    headers = {"Authorization": f"Bearer {token}"}
 
     # /recode
-    url = 'http://text.com/index#'
+    url = 'http://localhost:6003/info'
+    data = {'file': 'mecha.mp4'}
+    response = requests.post(url, headers=headers, params=data)
+    if response.status_code == 200:
+        print('请求成功')
+        print(response.json())
+    else:
+        print('请求失败')
+        print(response.text)
+    return response.status_code
+
+
+def get_req():
+    headers = {"Cookie": f""}
+
+    url = 'http://localhost:6003/get_msg'
     response = requests.get(url, headers=headers)
     if response.status_code == 200:
         print('请求成功')
@@ -21,3 +37,9 @@ if __name__ == "__main__":
     else:
         print('请求失败')
         print(response.text)
+    return response.status_code
+
+
+if __name__ == "__main__":
+    get_req()
+    #post_req()
